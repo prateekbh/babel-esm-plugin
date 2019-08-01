b0ff12e34b6fbf582ddb7be180cbbe1594caceb9
Janicklas Ralph James <janicklas@google.com>
2019-08-01 12:55:20 -0700

    Updating the new files in parent chunks. Adding additional error handling

--- src/index.js
+++ src/index.js
@@ -122,17 +122,42 @@ class BabelEsmPlugin {
             );
 
           childCompiler.runAsChild((err, entries, childCompilation) => {
-            if (!err) {
-              compilation.assets = Object.assign(
-                childCompilation.assets,
-                compilation.assets,
-              );
-              compilation.namedChunkGroups = Object.assign(
-                childCompilation.namedChunkGroups,
-                compilation.namedChunkGroups,
-              );
+            if (err) {
+              return childProcessDone(err);
             }
-            err && compilation.errors.push(err);
+
+            if (childCompilation.errors.length > 0) {
+              return childProcessDone(childCompilation.errors[0]);
+            }
+
+            compilation.assets = Object.assign(
+              childCompilation.assets,
+              compilation.assets,
+            );
+
+            compilation.namedChunkGroups = Object.assign(
+              childCompilation.namedChunkGroups,
+              compilation.namedChunkGroups,
+            );
+
+            const childChunkFileMap = childCompilation.chunks.reduce(
+              (chunkMap, chunk) => {
+                chunkMap[chunk.name] = chunk.files;
+                return chunkMap;
+              },
+              {},
+            );
+
+            compilation.chunks.forEach(chunk => {
+              const childChunkFiles = childChunkFileMap[chunk.name];
+
+              if (childChunkFiles) {
+                chunk.files.push(
+                  ...childChunkFiles.filter(v => !chunk.files.includes(v)),
+                );
+              }
+            });
+
             childProcessDone();
           });
         },
